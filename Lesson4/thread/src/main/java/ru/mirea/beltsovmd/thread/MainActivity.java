67abb94cdbdb9a70a01916f9df16c451f5d68ee7
MatvejBeltsov <matvej.beltsov@gmail.com>
2024-03-29 15:12:56 +0300

    4

--- Lesson4/thread/src/main/java/ru/mirea/beltsovmd/thread/MainActivity.java
+++ Lesson4/thread/src/main/java/ru/mirea/beltsovmd/thread/MainActivity.java
@@ -2,6 +2,7 @@ package ru.mirea.beltsovmd.thread;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,82 +15,94 @@ import java.util.concurrent.Executors;
 import ru.mirea.beltsovmd.thread.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
-    private int counter = 0;
+    private ActivityMainBinding binding = null;
+    private int thread_id_counter = 0;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Найти TextView для отображения информации о главном потоке
-        TextView infoTextView = findViewById(R.id.infoTextView);
-
-        // Получить текущий поток
         Thread mainThread = Thread.currentThread();
 
-
-        infoTextView.setText("Имя текущего потока: " + mainThread.getName());
-
-
-        mainThread.setName("МОЙ НОМЕР ГРУППЫ: 04, НОМЕР ПО СПИСКУ: 06, МОЙ ЛЮБИИМЫЙ ФИЛЬМ: Волк с Уолл-Стрит");
-        infoTextView.append("\n Новое имя потока: " + mainThread.getName());
-
-        // Вывести стек вызовов потока в LogCat
+        final String oldThreadName = mainThread.getName();
+        mainThread.setName("МОЙ НОМЕР ГРУППЫ: 04, НОМЕР ПО СПИСКУ: 6, МОЙ ЛЮБИИМЫЙ ФИЛЬМ: ВОЛК С УОЛЛ-СТРИТ");
+        final String newThreadName = mainThread.getName();
+        binding.mainThreadInfo.setText(
+                String.format("Имя текущего потока: %s\n", oldThreadName) +
+                        String.format("Новое имя потока: %s\n", newThreadName)
+        );
         Log.d(MainActivity.class.getSimpleName(), "Stack: " + Arrays.toString(mainThread.getStackTrace()));
-        binding.buttonMirea.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new Thread(new Runnable() {
-                    public void run() {
-                        int numberThread = counter++;
-                        Log.d("ThreadProject", String.format("Запущен поток No %d студентом группы No %s номер по списку No %d ", numberThread, "БСБО-04-22", 6));
-                        long endTime = System.currentTimeMillis() + 20 * 1000;
-                        while (System.currentTimeMillis() < endTime) {
-                            synchronized (this) {
-                                try {
-                                    wait(endTime - System.currentTimeMillis());
-                                    Log.d(MainActivity.class.getSimpleName(), "Endtime: " + endTime);
-                                } catch (Exception e) {
-                                    throw new RuntimeException(e);
+    }
+    public void OnStartBackgroundCalculationButtonPressed(View v) {
+        try {
+            final int days = Integer.parseUnsignedInt(binding.inputDaysPanel.getText().toString());
+            final int lessons = Integer.parseUnsignedInt(binding.inputLessonsPanel.getText().toString());
+
+
+            final Thread backround_worker = new Thread() {
+                @Override
+                public void run() {
+                    synchronized (this) {
+                        String calculation_result = "";
+                        try {
+                            Log.i(MainActivity.class.getSimpleName(), "Start background calculation");
+                            wait(2000);
+                            final float result = ((float) lessons) / days;
+                            Log.i(MainActivity.class.getSimpleName(), "Finish background calculation");
+                            calculation_result = String.format("Average lessons per day: %.2f", result);
+                        } catch (InterruptedException e) {
+                            calculation_result = "Calculation was interrupted...";
+                        } finally {
+                            final String formatted_result = String.format("Calculation result: %s", calculation_result);
+                            binding.calculationResult.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    binding.calculationResult.setText(formatted_result);
                                 }
-                            }
-                            Log.d("ThreadProject", "Выполнен поток No " + numberThread);
+                            });
                         }
                     }
-                }).start();
-            }
-        });
-
-        binding.calculateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculateAveragePairs();
-            }
-        });
+                }
+            };
+            backround_worker.start();
+        } catch(NumberFormatException e) {
+            binding.calculationResult.setText(String.format("Calculation result: %s", "Invalid input"));
+        }
     }
-
-    private void calculateAveragePairs() {
-        // Получение введенных пользователем значений
-        int totalPairs = Integer.parseInt(binding.totalPairsEditText.getText().toString());
-        int totalDays = Integer.parseInt(binding.totalDaysEditText.getText().toString());
-
-        // Выполнение вычислений в фоновом потоке
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.execute(new Runnable() {
+    public void OnStartBlockingCalculationButtonPressed(View v) {
+        final long endTime = System.currentTimeMillis() + 20 * 1000;
+        while (System.currentTimeMillis() < endTime) {
+            synchronized(this){
+                try {
+                    wait(endTime - System.currentTimeMillis());
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+    public void OnStartBlockingCalculationInBackgroundThreadButtonPressed(View v) {
+        new Thread(new Runnable() {
             @Override
             public void run() {
-                // Подсчет среднего количества пар в день
-                double averagePairs = (double) totalPairs / totalDays;
-
-                // Отображение результата в TextView в главном потоке
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        binding.resultTextView.setText("Average pairs per day: " + averagePairs);
+                final int numberThread = ++thread_id_counter;
+                Log.d("ThreadProject", String.format("Запущен поток № %d студентом группы № %s номер по списку № %d ", numberThread, "БСБО-04-22", 6));
+                long endTime = System.currentTimeMillis() + 20 * 1000;
+                while (System.currentTimeMillis() < endTime) {
+                    synchronized (this) {
+                        try {
+                            wait(endTime - System.currentTimeMillis());
+                            Log.d(MainActivity.class.getSimpleName(), "Endtime: " + endTime);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
                     }
-                });
+                    Log.d("ThreadProject", "Выполнен поток № " + numberThread);
+                }
             }
-        });
+        }).start();
     }
 }
